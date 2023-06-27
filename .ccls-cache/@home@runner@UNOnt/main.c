71e13685d9a29ff577d5f07abcc208effa8df136
AlexArz22 <127635481+AlexArz22@users.noreply.github.com>
2023-06-27 22:00:56 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@UNOnt/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+#include "Map.h"
+#include <time.h>
+#include <math.h>
+#include <stdbool.h>
+
+
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
