110019a1591786ec30fa04068aa8327d40b82a61
lostintothevoid <127423332+lostintothevoid@users.noreply.github.com>
2023-06-27 22:21:59 +0000

    realizacion de las estructuras para el juego

--- .ccls-cache/@home@runner@UNOnt/main.c
+++ .ccls-cache/@home@runner@UNOnt/main.c
@@ -8,6 +8,26 @@
 #include <math.h>
 #include <stdbool.h>
 
+//Estructuras
+typedef struct{
+  int numero; //En caso de que la carta sea numérica, llevará su número, en de que no, será -1;
+  int codigo; //Es el código específico para las cartas en general
+  int color; //El color de la carta va de 100 en 100. En caso de que no tenga color, este será 0.
+  int clave;
+}tipoCarta;
+
+typedef struct{
+  int id;//NÚMERO QUE LE CORRESPONDE AL ORDEN DE INGRESO DE NOMBRE
+  char jugador[100]; //NOMBRE DEL JUGADOR
+  List* cartasJugador; // DE LA BARAJA DEL JUGADOR
+}tipoJugador;
+
+typedef struct{
+  tipoCarta carta;
+  int cont; //Contador de veces que la carta está en el mapa de cartas
+}tipoMapa;
+
+
 int main(void) {
   List *listaJugadores = createList();
   Map *mapa = createMap(is_equal_int);
